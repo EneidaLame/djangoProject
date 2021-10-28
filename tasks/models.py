3491ca473f3ae348100e1aca59fc7c101022e433
eneidalame <eneida_lame@hotmail.com>
2021-10-28 19:48:59 +0300

    first steps of the projects

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=140)
+    description = models.TextField(null=True)
+    date_posted = models.DateTimeField()
+    completed = models.BooleanField(default=False)
